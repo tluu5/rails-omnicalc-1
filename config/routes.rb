0b90afc88736b4b96e79e987c1d47efd8a86f5c6
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2024-09-23 18:35:07 +0000

    commit to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  
+  get("/", { :controller => "zebra", :action => "homepage" })
+  get("/square/new", { :controller => "zebra", :action => "new_square_calc" })
+  get("/square/results", { :controller => "zebra", :action => "square_results" })
+  get("/square_root/new", { :controller => "zebra", :action => "new_square_root_calc" })
+  get("/square_root/results", { :controller => "zebra", :action => "square_root_results" })
+  get("/payment/new", { :controller => "zebra", :action => "new_payment_calc" })
+  get("/payment/results", { :controller => "zebra", :action => "payment_results" })
+  get("/random/new", { :controller => "zebra", :action => "new_random_calc" })
+  get("/random/results", { :controller => "zebra", :action => "random_results" })
 end
