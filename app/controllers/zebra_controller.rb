da4c543cb12be837f27f394e9826022bedac16cf
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2024-10-09 21:21:10 +0000

    Run Rubocop

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,51 +1,51 @@
 class ZebraController < ApplicationController
   def new_square_calc
-    render({ :template => "omnicalc-1_templates/new_square_calc"})
+    render({ template: 'omnicalc-1_templates/new_square_calc' })
   end
 
   def square_results
-    @the_num = params.fetch("user_number").to_f
-    @the_result = @the_num ** 2
-    render({ :template => "omnicalc-1_templates/square_results"})
+    @the_num = params.fetch('user_number').to_f
+    @the_result = @the_num**2
+    render({ template: 'omnicalc-1_templates/square_results' })
   end
 
   def new_square_root_calc
-    render({ :template => "omnicalc-1_templates/new_square_root_calc"})
+    render({ template: 'omnicalc-1_templates/new_square_root_calc' })
   end
 
   def square_root_results
-    @the_num = params.fetch("user_number").to_f
-    @the_result = @the_num ** 0.5
-    render({ :template => "omnicalc-1_templates/square_root_results"})
+    @the_num = params.fetch('user_number').to_f
+    @the_result = @the_num**0.5
+    render({ template: 'omnicalc-1_templates/square_root_results' })
   end
 
   def new_payment_calc
-    render({ :template => "omnicalc-1_templates/new_payment_calc"})
+    render({ template: 'omnicalc-1_templates/new_payment_calc' })
   end
 
   def payment_results
-    apr = params.fetch("apr_num").to_f
-    @apr_percent = apr.to_fs(:percentage, {:precision => 4})
-    @years = params.fetch("years_num").to_i
-    principal = params.fetch("principal_num").to_f
+    apr = params.fetch('apr_num').to_f
+    @apr_percent = apr.to_fs(:percentage, { precision: 4 })
+    @years = params.fetch('years_num').to_i
+    principal = params.fetch('principal_num').to_f
     @principal_dollar = principal.to_fs(:currency)
     r = apr / 100 / 12
     n = r * principal
-    d = 1 - (1 + r) ** -(@years * 12)
+    d = 1 - (1 + r)**-(@years * 12)
     pmt = n / d
     @pmayt = pmt.to_fs(:currency)
-    render({ :template => "omnicalc-1_templates/payment_results"})
+    render({ template: 'omnicalc-1_templates/payment_results' })
   end
 
   def new_random_calc
-    render({ :template => "omnicalc-1_templates/new_random_calc"})
+    render({ template: 'omnicalc-1_templates/new_random_calc' })
   end
 
   def random_results
-    @min = params.fetch("min_num").to_f
-    @max = params.fetch("max_num").to_f
+    @min = params.fetch('min_num').to_f
+    @max = params.fetch('max_num').to_f
     random_float = rand(@min..@max)
     @random_num = random_float.round(15)
-    render({ :template => "omnicalc-1_templates/random_results"})
+    render({ template: 'omnicalc-1_templates/random_results' })
   end
 end
